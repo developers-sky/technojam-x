ac045613a60fc4281af4b475b57f5fe8cf26dae8
anurag-bit <singh.ask@outlook.com>
2024-03-03 10:40:20 +0530

    Milestone: Completion of Blog frontend and CMS backend

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -37,6 +37,8 @@ import Link from "next/link";
 
 export default function Home() {
     const [isLoaded, setIsLoaded] = useState(false);
+    const [posts, setPosts] = useState([]);
+
 
     useEffect(() => {
         setIsLoaded(true);
@@ -48,54 +50,76 @@ export default function Home() {
         useCdn: true, // `false` if you want to ensure fresh data
     });
 
+
+    const postQuery =  `*[_type == "Post"]{
+        title,
+        shortDescription,
+        author->{
+            name
+        }
+    }`;// Replace with the actual value of postQuery
+
+    useEffect(() => {
+        client
+            .fetch(postQuery)
+            .then((data) => setPosts(data))
+            .catch(console.error);
+    }, []);
+
     const Skeleton = () => (
         <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
     );
-    const items = [
-        {
-            title: "The Dawn of Innovation",
-            description: "Explore the birth of groundbreaking ideas and inventions.",
-            header: <Skeleton />,
-            icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-        },
-        {
-            title: "The Digital Revolution",
-            description: "Dive into the transformative power of technology.",
-            header: <Skeleton />,
-            icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-        },
-        {
-            title: "The Art of Design",
-            description: "Discover the beauty of thoughtful and functional design.",
-            header: <Skeleton />,
-            icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-        },
-        {
-            title: "The Power of Communication",
-            description:
-                "Understand the impact of effective communication in our lives.",
-            header: <Skeleton />,
-            icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-        },
-        {
-            title: "The Pursuit of Knowledge",
-            description: "Join the quest for understanding and enlightenment.",
-            header: <Skeleton />,
-            icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-        },
-        {
-            title: "The Joy of Creation",
-            description: "Experience the thrill of bringing ideas to life.",
-            header: <Skeleton />,
-            icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-        },
-        {
-            title: "The Spirit of Adventure",
-            description: "Embark on exciting journeys and thrilling discoveries.",
-            header: <Skeleton />,
-            icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-        },
-    ];
+    // const items = [
+    //     {
+    //         title: "The Dawn of Innovation",
+    //         description: "Explore the birth of groundbreaking ideas and inventions.",
+    //         header: <Skeleton />,
+    //         icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    //     },
+    //     {
+    //         title: "The Digital Revolution",
+    //         description: "Dive into the transformative power of technology.",
+    //         header: <Skeleton />,
+    //         icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    //     },
+    //     {
+    //         title: "The Art of Design",
+    //         description: "Discover the beauty of thoughtful and functional design.",
+    //         header: <Skeleton />,
+    //         icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    //     },
+    //     {
+    //         title: "The Power of Communication",
+    //         description:
+    //             "Understand the impact of effective communication in our lives.",
+    //         header: <Skeleton />,
+    //         icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    //     },
+    //     {
+    //         title: "The Pursuit of Knowledge",
+    //         description: "Join the quest for understanding and enlightenment.",
+    //         header: <Skeleton />,
+    //         icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+    //     },
+    //     {
+    //         title: "The Joy of Creation",
+    //         description: "Experience the thrill of bringing ideas to life.",
+    //         header: <Skeleton />,
+    //         icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+    //     },
+    //     {
+    //         title: "The Spirit of Adventure",
+    //         description: "Embark on exciting journeys and thrilling discoveries.",
+    //         header: <Skeleton />,
+    //         icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+    //     },
+    // ];
+    const items = posts.map((post) => ({
+        title: post.title,
+        description: post.shortDescription,
+        header: <Skeleton />,
+        icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+      }));
 
     return (
         <>
